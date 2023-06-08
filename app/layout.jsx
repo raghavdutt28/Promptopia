9ac16e06cc64aae3e4e360ce9b0f0ff46c562e53
Raghav Dutta <raghav.l.ldutt@gmail.com>
2023-06-08 08:36:09 +0530

    fixed typo

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
 import '@styles/globals.css';
 import Nav from '@components/Nav';
-import Provider from '@components/Provider';
+import Provider from '@components/provider';
 
 export const metadata = {
     title: "Promptopia",
